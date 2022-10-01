6d32a1b831ed1c8006774333391d0a90eaec9c1b
thexword <thexwordchs@gmail.com>
2022-10-01 16:55:10 +0800

    fix error

--- csource/1_20.c
+++ csource/1_20.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 #define TAB_LENGTH 8
+#define IS_TAB 1
+#define NOT_TAB 0
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int c;
+    int status = NOT_TAB;
 
     while ((c = getchar()) != '\n') {
         if (c == '\t') {
-            for (int i = 0; i < TAB_LENGTH; i++)
-            {
-                putchar(' ');
+            if (status == NOT_TAB) {
+                for (int i = 0; i < TAB_LENGTH; i++) {
+                    putchar(' ');
+                }
             }
+            status = IS_TAB;
         } else {
             putchar(c);
+            status = NOT_TAB;
         }
     }
 
