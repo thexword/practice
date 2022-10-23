673ed08afc615f63cedb6bc90b3463be668088b7
thexword <thexwordchs@gmail.com>
2022-10-23 15:57:59 +0800

    Add userinput shell script

--- shell/userinput.sh
+++ shell/userinput.sh
@@ -1,2 +1,23 @@
-#！/bin/bash
+#!/bin/bash
 
+echo "======> input options and parameters"
+
+set -- $(getopt a:bc "$@")
+echo
+
+while [ -n "$1" ]; do
+    case "$1" in
+        -a) echo "option: a with paramter value: $2"
+            shift;;
+        -b) echo "option: b";;
+        -c) echo "option: c";;
+        --) shift
+            break;;
+        *) echo "parameter: $1";;
+    esac
+    shift
+done
+
+for var in $@; do
+    echo "parameter: $var"
+done
