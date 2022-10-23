be5e1cda2506cd2f0129c1d6b62ae04c070859e2
thexword <thexwordchs@gmail.com>
2022-10-23 10:35:26 +0800

    Add shell scripts

--- shell/helloScript.sh
+++ shell/helloScript.sh
@@ -1,6 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Hello Bash Script" > file.txt
 
-var=$(echo "scale=4;3.44/5" | bc)
+var=$(echo "scale=2;3.44/5" | bc)
 echo $var
+
+var1=$(bc << EOF
+scale=5
+a=3.24
+b=4
+a/b
+EOF
+)
+echo $var1
